1cd91fdf073c75006d0b41ada156e9ae93504afe
orels1 <orel-s1@yandex.ru>
2018-03-22 04:13:43 +0300

    added basic CLI support for collections listing

---
+++ cli/commands/users/collections.js
@@ -0,0 +1,45 @@
+module.exports = (() => {
+
+  'use strict';
+
+  const Command = require('cmnd').Command;
+  const { stripIndents } = require('common-tags');
+  const users = require('../../../src/users');
+
+  class Users extends Command {
+
+    constructor() {
+
+      super('users', 'collections');
+
+    }
+
+    help() {
+
+      return {
+        description: 'Get user\'s collections games',
+        args: ['username']
+      };
+
+    }
+
+    formatter(user, collections) {
+      return (stripIndents`
+        👤  ${user}'s collections
+        👾  ${collections.length} total
+
+        ${collections.map(collection => ` - ${collection.name}`).join('\n')}
+      `);
+    }
+
+    run({ args }, callback) {
+      users(args[0]).collections()
+        .then(collections => callback(null, this.formatter(args[0], collections.get())))
+        .catch(e => callback(new Error(e.message)))
+    }
+
+  }
+
+  return Users;
+
+})();
