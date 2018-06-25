bd33e9beb212aa6d44070023a2cceafbe6f4f637
orels1 <orel-s1@yandex.ru>
2018-06-25 23:22:43 +0300

    removed CLI, not used anymore

--- cli/bin.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-'use strict';
-
-const CommandLineInterface = require('cmnd').CommandLineInterface;
-const CLI = new CommandLineInterface();
-
-CLI.load(__dirname, './commands');
-CLI.run(process.argv.slice(2));
